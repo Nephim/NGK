6917baf49dd4dabc8f9caaaa3c76935339c3cc7b
Your Name <you@example.com>
2022-11-07 16:33:57 +0100

    Did Exercise 3 WebAPI part1.

--- restAPI/main.cpp
+++ restAPI/main.cpp
@@ -4,6 +4,63 @@
 
 #include <json_dto/pub.hpp>
 
+struct place_t
+{
+	place_t() = default;
+
+	place_t(std::string placeName, float lat, float lon) : 
+	m_placeName{ std::move( placeName ) },
+	m_lat{ std::move( lat ) },
+	m_lon{ std::move( lon ) }
+	{}
+
+	template < typename JSON_IO > void
+	json_io( JSON_IO & io )
+	{
+		io
+			& json_dto::mandatory( "placeName", m_placeName )
+			& json_dto::mandatory( "latitude", m_lat )
+			& json_dto::mandatory( "longitude", m_lon );
+	}
+
+	std::string m_placeName;
+	double m_lat, m_lon;
+};
+
+struct weatherRegistraiton_t
+{
+	weatherRegistraiton_t() = default;
+
+	weatherRegistraiton_t(std::int64_t id, std::int64_t date, std::string time, place_t place, double temperature, double humidity) :
+	m_id{ std::move( id ) },
+	m_date{ std::move( date ) },
+	m_time{ std::move( time ) },
+	m_place{ std::move( place ) },
+	m_temperature{ std::move( temperature ) },
+	m_humidity{ std::move( humidity ) }
+	{}
+
+	template < typename JSON_IO >
+	void
+	json_io( JSON_IO & io )
+	{
+		io
+			& json_dto::mandatory( "id", m_id )
+			& json_dto::mandatory( "date", m_date )
+			& json_dto::mandatory( "time", m_time )
+			& json_dto::mandatory( "place", m_place )
+			& json_dto::mandatory( "temperature", m_temperature )
+			& json_dto::mandatory( "humidity", m_humidity );
+	}
+
+	std::int64_t m_id;
+	std::int64_t m_date;
+	std::string m_time;
+	place_t m_place;
+	double m_temperature;
+	double m_humidity;
+};
+
 struct book_t
 {
 	book_t() = default;
@@ -28,10 +85,59 @@ struct book_t
 	std::string m_title;
 };
 
+using weatherStation_t = std::vector< weatherRegistraiton_t >;
+
 using book_collection_t = std::vector< book_t >;
 
 namespace rr = restinio::router;
 using router_t = rr::express_router_t<>;
+
+class weatherStationHandler_t
+{
+public:
+	explicit weatherStationHandler_t(weatherStation_t & registrations) :
+	m_registrations{ registrations }
+	{}
+	
+	auto onWeatherList(const restinio::request_handle_t& req, rr::route_params_t ) const
+	{
+		auto resp = init_resp( req->create_response() );
+
+		resp.set_body("Weather registrations:\n");
+
+		for( std::size_t i = 0; i < m_registrations.size(); ++i )
+		{
+			const auto & w = m_registrations[ i ];
+			resp.append_body(json_dto::to_json(w) + "\n");
+		}
+
+		return resp.done();
+	}
+
+private:
+	weatherStation_t& m_registrations;
+
+	template < typename RESP >
+	static RESP
+	init_resp( RESP resp )
+	{
+		resp
+			.append_header( "Server", "RESTinio sample server /v.0.6" )
+			.append_header_date_field()
+			.append_header( "Content-Type", "text/plain; charset=utf-8" );
+
+		return resp;
+	}
+
+	template < typename RESP >
+	static void
+	mark_as_bad_request( RESP & resp )
+	{
+		resp.header().status_line( restinio::status_bad_request() );
+	}
+};
+
+
 
 class books_handler_t
 {
@@ -208,6 +314,34 @@ private :
 	}
 };
 
+auto weatherHandler(weatherStation_t & weatherStation)
+{
+	auto router = std::make_unique< router_t >();
+	auto handler = std::make_shared< weatherStationHandler_t >( std::ref(weatherStation) );
+
+	auto by = [&]( auto method ) {
+		using namespace std::placeholders;
+		return std::bind( method, handler, _1, _2 );
+	};
+
+	auto method_not_allowed = []( const auto & req, auto ) {
+			return req->create_response( restinio::status_method_not_allowed() )
+					.connection_close()
+					.done();
+	};
+
+	// Handlers for '/' path.
+	router->http_get( "/", by( &weatherStationHandler_t::onWeatherList ) );
+
+	// Disable all other methods for '/'.
+	router->add_handler(
+			restinio::router::none_of_methods(
+			restinio::http_method_get(), restinio::http_method_post() ),
+			"/", method_not_allowed );
+
+	return router;
+}
+
 auto server_handler( book_collection_t & book_collection )
 {
 	auto router = std::make_unique< router_t >();
@@ -282,10 +416,15 @@ int main()
 			{ "B. Stroustrup", "The C++ Programming Language" }
 		};
 
+		weatherStation_t weatherStation{
+			{1, 20211105, "12:15", {"Aarhus N", 13.692, 19.438}, 13.1, 70},
+			{2, 20211105, "12:15", {"Aarhus N", 13.692, 19.438}, 13.1, 70}
+		};
+
 		restinio::run(
 			restinio::on_this_thread< traits_t >()
 				.address( "localhost" )
-				.request_handler( server_handler( book_collection ) )
+				.request_handler( weatherHandler( weatherStation ) )
 				.read_next_http_message_timelimit( 10s )
 				.write_http_response_timelimit( 1s )
 				.handle_request_timeout( 1s ) );
